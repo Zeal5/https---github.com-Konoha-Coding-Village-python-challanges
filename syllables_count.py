7ced7b41e30e6ef2bf08f7e221be3cfdc38a7182
Zeal5 <tassadiqshakeel5@gmail.com>
2022-02-13 04:54:24 +0500

    syllables counter

---
+++ syllables_count.py
@@ -0,0 +1,12 @@
+def count(s):
+    cal = 1
+    for i in s:
+        if i =='-':
+            cal += 1
+    return cal
+    
+    
+    
+    
+count('met-a-phor')
+    
--- test.py
+++
@@ -1,2 +0,0 @@
-jsdfuij
-fghgfhg
