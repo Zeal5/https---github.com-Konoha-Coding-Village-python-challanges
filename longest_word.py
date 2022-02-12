690ea5b84a3cd43a9b74b16e48b153058ed7e0e9
Zeal5 <tassadiqshakeel5@gmail.com>
2022-02-13 01:55:22 +0500

    longest_word

---
+++ longest_word.py
@@ -0,0 +1,26 @@
+import string
+alphabets = string.ascii_letters
+
+
+def LongestWord(sen):
+    new_sen = sen.split()
+    new_word = ''
+    for letters in new_sen:
+        new_word += ' '
+          
+        for letter in letters:
+            
+            if letter in alphabets:
+                new_word += letter
+            else:
+                pass
+    max = 0    
+    for i in new_word.split():
+        
+        if len(i)>max:
+            max = len(i)
+            x = i
+    
+    return(x)        
+       
+print(LongestWord(input()))
