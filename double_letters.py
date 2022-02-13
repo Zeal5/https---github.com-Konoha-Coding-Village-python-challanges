d9cff11f8cd9da10709a347fd2276c20e99e787d
Zeal5 <tassadiqshakeel5@gmail.com>
2022-02-13 06:44:56 +0500

    double_letters

---
+++ double_letters.py
@@ -0,0 +1,16 @@
+def double_letters(s):
+    count = -2
+    
+    
+    for i in s:
+        count += 1
+        if s[count] == i:
+            return i
+
+
+double_letters('ahhgbggn')
+
+
+
+    
+    
